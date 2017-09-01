365d52e4afe5e6eef6a1587c9c8a2ade088e9d82
Shana L Moore <shana.lavina.moore@gmail.com>
2017-09-01 08:22:57 -0700

    cleanup code

--- speed/speed/speed.c
+++ speed/speed/speed.c
@@ -5,34 +5,28 @@
 //  Copyright © 2017 Shana Moore. All rights reserved.
 //
 
-/**
- *   speed.c
- *
- *   Created by ....
- */
 #include <stdio.h>
+# define METERS_PER_MILE 1600 // conversion constant
+# define SECONDS_PER_HOUR 3600 // conversion constant
 
 int main(void) {
     // Declare variables with appropriate data types
-    int METERS_PER_MILE = 1600,
-        SECONDS_PER_HOUR = 3600;
-    
     FILE *fp = NULL;
     
     float distance = 0, hours = 0, meters = 0, seconds = 0,
           miles_per_hour = 0, meters_per_second = 0;
   
-    /* Assign values */
+    // Assign values
     distance = 425.5f;
     hours = 7.5f;
     
     // Convert the distance to meters
     meters = distance * METERS_PER_MILE;
     
-    /* Convert the hours to seconds */
+    // Convert the hours to seconds
     seconds = hours * SECONDS_PER_HOUR;
     
-    /* Output to file */
+    // Output to file + handle error case
     fp = fopen("csis.txt", "w");
     if (NULL == fp) {
         perror("opening csis.txt failed");
@@ -43,12 +37,12 @@ int main(void) {
     miles_per_hour = distance / hours;
     meters_per_second = meters / seconds;
     
-    /* Compute speed of car in miles per hour and output speed */
+    // Display speed of car in miles per hour
     printf("The car's speed is %.2f miles/hour.\n", miles_per_hour);
     fprintf(fp, "The car's speed is %.2f miles/hour.\n", miles_per_hour);
     
     
-    /* Compute speed of car in meters per second and output speed */
+    // Display speed of car in meters per second
     printf("The car's speed is %.2f meters/second.\n", meters_per_second);
     fprintf(fp, "The car's speed is %.2f meters/second.\n", meters_per_second);
 
